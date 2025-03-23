b0cdea5d5a5e3e9dbacb17a3ee3d8193bd11052a
ToseebNadaf <toseebnadaf@gmail.com>
2025-03-23 15:48:40 +0530

    Minor fix

--- bot.js
+++ bot.js
@@ -51,7 +51,7 @@ app.get("/", (req, res) => {
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
 
 bot.launch();
